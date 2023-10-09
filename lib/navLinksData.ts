b33360df27423b21149db0325e86b3d8866dfdc5
pountzas <nikos@pountzas.gr>
2023-10-09 23:49:33 +0300

    create nav link lib file

---
+++ lib/navLinksData.ts
@@ -0,0 +1,16 @@
+const navLinksData = [
+  {
+    name: "Features",
+    url: "/Features"
+  },
+  {
+    name: "About",
+    url: "/About"
+  },
+  {
+    name: "Contact",
+    url: "/Contact"
+  }
+];
+
+export default navLinksData;
