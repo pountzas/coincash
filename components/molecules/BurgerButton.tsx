a9a48eaa5aa5d60f7e106967bbcf46593ac61b6b
pountzas <nikos@pountzas.gr>
2023-10-13 00:00:25 +0300

    AnimatePresence on burger button

--- components/molecules/BurgerButton.tsx
+++ components/molecules/BurgerButton.tsx
@@ -2,7 +2,7 @@
 import navLinksData from "@/lib/navLinksData";
 import { useState } from "react";
 import NavbarLink from "../atoms/NavbarLink";
-import { motion } from "framer-motion";
+import { motion, AnimatePresence } from "framer-motion";
 import HamburgerIcon from "@/public/icons/HamburgerIcon";
 import CloseIcon from "@/public/icons/CloseIcon";
 
@@ -14,24 +14,26 @@ function BurgerButton() {
       className="relative md:hidden"
       onClick={() => setIsBurgerOpen(!isBurgerOpen)}
     >
-      {isBurgerOpen && (
-        <motion.ul
-          initial={{ opacity: 0, x: 100 }}
-          animate={{ opacity: 1, x: 0 }}
-          exit={{ opacity: 0, x: 100 }}
-          className="absolute top-6 right-0 bg-[#0066FF] rounded-md"
-        >
-          <NavbarLink title="Home" href="/" type="burgerTop" />
-          {navLinksData.map((link) => (
-            <NavbarLink
-              key={link.name}
-              title={link.name}
-              href={link.url}
-              type="burger"
-            />
-          ))}
-        </motion.ul>
-      )}
+      <AnimatePresence>
+        {isBurgerOpen && (
+          <motion.ul
+            initial={{ opacity: 0, x: 100 }}
+            animate={{ opacity: 1, x: 0 }}
+            exit={{ opacity: 0, x: 100 }}
+            className="absolute top-6 right-0 bg-[#0066FF] rounded-md"
+          >
+            <NavbarLink title="Home" href="/" type="burgerTop" />
+            {navLinksData.map((link) => (
+              <NavbarLink
+                key={link.name}
+                title={link.name}
+                href={link.url}
+                type="burger"
+              />
+            ))}
+          </motion.ul>
+        )}
+      </AnimatePresence>
       {!isBurgerOpen ? <HamburgerIcon /> : <CloseIcon />}
     </div>
   );
